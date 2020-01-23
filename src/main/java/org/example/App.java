bfcec5dc4137d997fef45003982663108f72a9a8
aleksandra-gonta <aleksandra.gonta@gmail.com>
2020-01-23 12:15:13 +0100

    Create car validator classes

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,16 @@
 package org.example;
 
 
-public class App 
+import org.example.service.CarsService;
+
+public class App
 {
     public static void main( String[] args )
     {
+        final String JSON_FILENAME = "cars.json";
+        CarsService carsService = new CarsService(JSON_FILENAME);
 
+        System.out.println(carsService);
 
     }
 }
--- src/main/java/org/example/service/CarsService.java
+++ src/main/java/org/example/service/CarsService.java
@@ -3,6 +3,8 @@ package org.example.service;
 import org.eclipse.collections.impl.collector.BigDecimalSummaryStatistics;
 import org.eclipse.collections.impl.collector.Collectors2;
 
+import org.example.converter.CarsJsonConverter;
+import org.example.exception.AppException;
 import org.example.model.Car;
 
 import org.example.model.data.MileageStatistics;
@@ -10,20 +12,47 @@ import org.example.model.data.PriceStatistics;
 import org.example.model.data.Statistics;
 import org.example.service.enums.Color;
 import org.example.service.enums.OrderBy;
+import org.example.validator.CarValidator;
 
 
 import java.math.BigDecimal;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class CarsService {
 
     private final Set<Car> cars;
 
-    public CarsService(Set<Car> cars) {
-        this.cars = cars;
+    public CarsService(String jsonFilename) {
+        this.cars = parseAndValidateCars(jsonFilename);
     }
 
+    private static Set<Car> parseAndValidateCars(String jsonFilename) {
+
+        if (jsonFilename == null) {
+            throw new AppException("json filename string is null");
+        }
+
+        AtomicInteger counter = new AtomicInteger(1);
+        CarValidator orderValidator = new CarValidator();
+
+
+        return new CarsJsonConverter(jsonFilename)
+                .fromJson()
+                .orElseThrow(() -> new AppException("cannot parse data from json file " + jsonFilename))
+                .stream()
+                .filter(car -> {
+                    var errors =orderValidator.validate(car);
+                    if (orderValidator.hasErrors()) {
+                        System.out.println("Validation errors for order no. " + counter.get() + ":");
+                        errors.forEach((field, message) -> System.out.println(field + ": " + message));
+                        System.out.println("-----------------------------------------------------");
+                    }
+                    counter.incrementAndGet();
+                    return !orderValidator.hasErrors();
+                }).collect(Collectors.toSet());
+    }
 
     public List<Car> sortBy(OrderBy orderBy, boolean descending) {
         List<Car> orderedCars = switch (orderBy) {
